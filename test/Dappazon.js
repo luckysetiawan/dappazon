986340ac8a51aea2373ce99040beca8f20b3b57c
luckysetiawan <luckysetiawan0802@gmail.com>
2024-06-15 17:13:20 +0700

    setup basic contract and test

--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,25 @@
 const { expect } = require("chai")
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
+  return ethers.parseUnits(n.toString(), 'ether');
 }
 
-describe("Dappazon", () => {
+describe('Dappazon', () => {
+  let deployer, buyer;
+  let dappazon;
 
+  beforeEach(async ()=> {
+    // Setup Accounts
+    [ deployer, buyer ] = await ethers.getSigners();
+
+    // Deploy contract
+    const DappazonFactory = await ethers.getContractFactory('Dappazon');
+    dappazon = await DappazonFactory.deploy();
+  })
+
+  describe('Deployment', () => {
+    it('Sets the owner', async () => {
+      expect(await dappazon.owner()).to.equal(deployer.address);
+    })
+  })
 })
