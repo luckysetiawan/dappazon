876955ef27a45d5888734fe7f810abf2051946c6
luckysetiawan <luckysetiawan0802@gmail.com>
2024-06-15 18:16:24 +0700

    add item listing and test

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -4,7 +4,42 @@ pragma solidity ^0.8.20;
 contract Dappazon {
     address public owner;
 
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    mapping(uint256 => Item) public items;
+
     constructor() {
         owner = msg.sender;
+    }
+
+    // List products
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+    ) public {
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        );
+
+        items[_id] = item;
     }
 }
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -4,6 +4,15 @@ const tokens = (n) => {
   return ethers.parseUnits(n.toString(), 'ether');
 }
 
+// Global constants for listing an item
+const ID = 1;
+const NAME = 'Shoes';
+const CATEGORY = 'Clothing';
+const IMAGE = 'https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg';
+const COST = tokens(1);
+const RATING = 4;
+const STOCK = 5;
+
 describe('Dappazon', () => {
   let deployer, buyer;
   let dappazon;
@@ -20,6 +29,27 @@ describe('Dappazon', () => {
   describe('Deployment', () => {
     it('Sets the owner', async () => {
       expect(await dappazon.owner()).to.equal(deployer.address);
+    })
+  })
+
+  describe('Listing', () => {
+    let transaction;
+
+    beforeEach(async ()=> {
+      transaction = await dappazon.connect(deployer).list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+      await transaction.wait();
+    })
+
+    it('Returns item attributes', async () => {
+      const item = await dappazon.items(1);
+
+      expect(item.id).to.equal(ID);
+      expect(item.name).to.equal(NAME);
+      expect(item.category).to.equal(CATEGORY);
+      expect(item.image).to.equal(IMAGE);
+      expect(item.cost).to.equal(COST);
+      expect(item.rating).to.equal(RATING);
+      expect(item.stock).to.equal(STOCK);
     })
   })
 })
