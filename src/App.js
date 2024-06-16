31c54cf1a4bc906dbd8a26014ab54b3eccdb842d
luckysetiawan <luckysetiawan0802@gmail.com>
2024-06-16 22:36:47 +0700

    integrate products data from the blockchain

--- src/App.js
+++ src/App.js
@@ -19,13 +19,39 @@ function App() {
 
   const [account, setAccount] = useState(null);
 
+  const [clothing, setCothing] = useState(null);
+  const [electronics, setElectronics] = useState(null);
+  const [toys, setToys] = useState(null);
+
+  const togglePop = () => {
+    console.log("Toggle Pop");
+  }
+
   const loadBlockchainData = async () => {
     const provider = new ethers.BrowserProvider(window.ethereum);
     setProvider(provider);
+
     const network = await provider.getNetwork();
 
     const dappazon = new ethers.Contract(config[network.chainId].dappazon.address, Dappazon, provider);
     setDappazon(dappazon);
+
+    // Load products
+    const items = [];
+
+    // OPTIMIZE: Should add get product amount function on the contract for the loop
+    for (let i = 1; i <= 9; i++) {
+      const item = await dappazon.items(i);
+      items.push(item);
+    }
+    
+    const clothing = items.filter((item) => item.category === 'clothing');
+    const electronics = items.filter((item) => item.category === 'electronics');
+    const toys = items.filter((item) => item.category === 'toys');
+
+    setCothing(clothing);
+    setElectronics(electronics);
+    setToys(toys);
   }
 
   useEffect(() => {
@@ -36,7 +62,15 @@ function App() {
     <div>
       <Navigation account={account} setAccount={setAccount} />
 
-      <h2>Welcome to Dappazon</h2>
+      <h2>Dappazon Best Sellers</h2>
+      
+      {electronics && clothing && toys && (
+        <>
+          <Section title={"Clothing & Jewelry"} items={clothing} togglePop={togglePop} />
+          <Section title={"Electronics & Gadgets"} items={electronics} togglePop={togglePop} />
+          <Section title={"Toys & Gaming"} items={toys} togglePop={togglePop} />
+        </>
+      )}
 
     </div>
   );
